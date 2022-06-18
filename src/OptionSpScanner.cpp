4e6107bfabd84b70824a7357b59ddf24a9c32064
SSYSS000 <jaessys@protonmail.com>
2022-06-18 13:05:34 +0300

    Implementation of OptionSpScanner

---
+++ src/OptionSpScanner.cpp
@@ -0,0 +1,83 @@
+#include <algorithm>
+#include <string>
+#include "OptionSpScanner.hpp"
+
+OptionSpScanner::OptionSpScanner(std::string short_opt_prefix,
+			 std::string long_opt_prefix,
+			 std::string end_of_opts) :
+	OptionScanner(std::move(short_opt_prefix), std::move(long_opt_prefix),
+		 std::move(end_of_opts))
+{
+
+}
+
+void OptionSpScanner::expect_value(std::string option_no_pfx)
+{
+	m_value_opts.insert(std::move(option_no_pfx));
+}
+
+bool OptionSpScanner::is_value_expected(char option_no_pfx) const
+{
+	return is_value_expected(std::string(1, option_no_pfx));
+}
+
+bool OptionSpScanner::is_value_expected(const std::string& option_no_pfx) const
+{
+	return m_value_opts.find(option_no_pfx) != m_value_opts.cend();
+}
+
+bool OptionSpScanner::scan_next(ArgumentStack& stack, std::vector<Option>& out)
+{
+	if (!m_opt_scanning)
+	{
+		return false;
+	}
+
+	std::string arg = std::move(stack.top());
+	stack.pop();
+
+	if (arg == m_end_of_opts)
+	{
+		m_opt_scanning = false;
+	}
+	else if (is_long_option(arg))
+	{
+		arg.erase(0, m_long_opt_prefix.length());
+		if (!stack.empty() && is_value_expected(arg))
+		{
+			out.emplace_back(std::move(arg), std::move(stack.top()));
+			stack.pop();
+		}
+		else
+		{
+			out.emplace_back(std::move(arg), "");
+		}
+	}
+	else if (is_short_option(arg))
+	{
+		arg.erase(0, m_short_opt_prefix.length());
+
+		for (auto it = arg.begin(); it != arg.end() - 1; ++it)
+		{
+			out.emplace_back(*it, "");
+		}
+
+		if (!stack.empty() && is_value_expected(arg.back()))
+		{
+			out.emplace_back(arg.back(), std::move(stack.top()));
+			stack.pop();
+		}
+		else
+		{
+			out.emplace_back(arg.back(), "");
+		}
+	}
+	else
+	{
+		// Put the argument back on the stack.
+		stack.push(std::move(arg));
+		return false;
+	}
+
+	return true;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,75 @@
 #include <iostream>
 #include "ArgumentScanner.hpp"
 #include "OptionEqScanner.hpp"
+#include "OptionSpScanner.hpp"
+#include "OptionMap.hpp"
+#include "OptionInfo.hpp"
 
-int main(int argc, char **argv)
+constexpr auto SHORT_OPT_PREFIX = "-";
+constexpr auto LONG_OPT_PREFIX = "--";
+constexpr auto END_OF_OPTS = "--";
+
+enum class MyOpt
+{
+	ADDRESS,
+	RETRY,
+	QUIT,
+	VERBOSITY,
+	HELP,
+	VERSION
+};
+
+std::map<MyOpt, OptionInfo> opt_info {
+		{MyOpt::ADDRESS, 		OptionInfo("a", "address", "the address to send to", "address")},
+		{MyOpt::RETRY, 			OptionInfo("r", "retry", "retry this many times", "retry_count")},
+		{MyOpt::QUIT, 			OptionInfo("q", "", "quit the app")},
+		{MyOpt::VERBOSITY, 		OptionInfo("v", "", "set verbosity level")},
+		{MyOpt::HELP,			OptionInfo("h", "help", "show help text")},
+		{MyOpt::VERSION,		OptionInfo("", "version", "show version")}
+};
+
+OptionMap<MyOpt> options_to_map(const std::vector<Option>& options)
+{
+	OptionMap<MyOpt> map;
+	for (const auto& o : options)
+	{
+		for (const auto& [id, info] : opt_info)
+		{
+			if (o.name() == info.short_opt || o.name() == info.long_opt)
+			{
+				map.add(id, o.value());
+			}
+		}
+	}
+	return map;
+}
+
+OptionMap<MyOpt> just_give_me_the_damn_map(int argc, char **argv)
 {
 	ArgumentStack stack = make_arg_stack(argc, argv);
 
-	OptionEqScanner opt_scanner;
-	ArgumentScanner scanner;
-	scanner.set_option_scanner(&opt_scanner);
+	OptionSpScanner opt_scanner(SHORT_OPT_PREFIX, LONG_OPT_PREFIX, END_OF_OPTS);
+
+	for(const auto& [id, info] : opt_info)
+	{
+		if (!info.value_name.empty())
+		{
+			opt_scanner.expect_value(info.short_opt);
+			opt_scanner.expect_value(info.long_opt);
+		}
+	}
+
+	ArgumentScanner scanner(&opt_scanner);
 	ScannedArguments scanned = scanner.scan(stack);
 
-	for (const auto& op : scanned.operands)
-	{
-		std::cout << "Operand: " << op << '\n';
-	}
+	return options_to_map(scanned.options);
+}
 
-	for (const auto& op : scanned.options)
-	{
-		std::cout << "Option: " << op.name() << " = " << op.value() << '\n';
-	}
+int main(int argc, char **argv)
+{
+	auto opt_map = just_give_me_the_damn_map(argc, argv);
+	std::cout << "verbosity: " << opt_map.values(MyOpt::VERBOSITY).size() << '\n';
+	std::cout << "retry: " << opt_map.get_value(MyOpt::RETRY) << '\n';
 
     return 0;
 }
