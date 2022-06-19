da470f1a66a483881c6adac62532255a9d737259
SSYSS000 <jaessys@protonmail.com>
2022-06-19 20:47:36 +0300

    Throw if short and long option prefixes are equal

--- src/OptionScanner.cpp
+++ src/OptionScanner.cpp
@@ -48,6 +48,12 @@ OptionScanner::OptionScanner(std::string short_opt_prefix,
     m_long_opt_prefix(std::move(long_opt_prefix)),
     m_end_of_opts(std::move(end_of_opts))
 {
+    if (m_short_opt_prefix == m_long_opt_prefix)
+    {
+        throw std::invalid_argument("Short and long option prefixes "
+                                    "may not be equal.");
+    }
+
     if (!(m_short_opt_prefix.length() < m_long_opt_prefix.length()))
     {
         throw std::invalid_argument("Short option prefix must be shorter than "
